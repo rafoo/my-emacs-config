567974d5cf8d5d1c8d975e4a0d127feeafa8906b
Raphaël Cauderlier <cauderlier@crans.org>
2013-01-15 10:40:52 +0100

    Package, org and coq configuration

--- .emacs.d/elisp/org-conf.el
+++ .emacs.d/elisp/org-conf.el
@@ -6,6 +6,8 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
+(require 'org-special-blocks)
+
 (require 'org-latex)
 (unless (boundp 'org-export-latex-classes)
   (setq org-export-latex-classes nil))
@@ -17,6 +19,42 @@
 ;;              '("article"
 ;;                "\\documentclass{article}"
 ;;                ("\\section{%s}" . "\\section*{%s}")))
+
+(add-to-list 'org-export-latex-classes
+             '("logic-article"
+               "\\documentclass[11pt]{article}
+[NO-DEFAULT-PACKAGES]
+\\usepackage{fixltx2e}
+\\usepackage{graphicx}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{wrapfig}
+\\usepackage{soul}
+\\usepackage{textcomp}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{latexsym}
+\\usepackage{amssymb}
+\\usepackage{hyperref}
+\\tolerance=1000
+\\usepackage{mathspec}
+\\setallmainfonts{FreeSerif}
+\\usepackage[french]{babel} 
+\\usepackage{bussproofs}
+\\newcommand{\\myUIC}[3]{\\mbox{\\AxiomC{#2}\\RightLabel{\\scriptsize(#1)}\\UnaryInfC{#3}\\DisplayProof}}
+\\newcommand{\\myBIC}[4]{\\mbox{\\AxiomC{#2}\\AxiomC{#3}\\RightLabel{\\scriptsize(#1)}\\BinaryInfC{#4}\\DisplayProof}}
+\\newcommand{\\myTIC}[5]{\\mbox{\\AxiomC{#2}\\AxiomC{#3}\\AxiomC{#4}\\RightLabel{\\scriptsize(#1)}\\TrinaryInfC{#5}\\DisplayProof}}
+\\newcommand{\\myQIC}[6]{\\mbox{\\AxiomC{#2}\\AxiomC{#3}\\AxiomC{#4}\\AxiomC{#5}\\RightLabel{\\scriptsize(#1)}\\QuaternaryInfC{#6}\\DisplayProof}}
+\\usepackage{setspace}
+\\usepackage{framed}
+\\newenvironment{infset}{\\begin{center}\\begin{framed}\\setstretch{3}}{\\end{framed}\\end{center}}
+"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
 
 
 (provide 'org-conf)
