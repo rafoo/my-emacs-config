130cdc4b8b726304077aeeb3e694ebfde24ca02c
Raphaël Cauderlier <cauderlier@crans.org>
2013-07-15 12:05:09 +0200

    [org-conf] passage à llncs

--- .emacs.d/elisp/org-conf.el
+++ .emacs.d/elisp/org-conf.el
@@ -24,8 +24,9 @@
 
 (add-to-list 'org-export-latex-classes
              '("logic-article"
-               "\\documentclass[11pt]{article}
+               "\\documentclass{llncs}
 [NO-DEFAULT-PACKAGES]
+\\usepackage{amsmath}
 \\usepackage{a4wide}
 \\usepackage{fixltx2e}
 \\usepackage{graphicx}
@@ -42,7 +43,7 @@
 \\tolerance=1000
 \\usepackage{mathspec}
 \\setallmainfonts{FreeSerif}
-\\usepackage[french]{babel} 
+%\\usepackage[french]{babel} 
 \\usepackage{bussproofs}
 \\newcommand{\\myUIC}[3]
   {\\mbox{
