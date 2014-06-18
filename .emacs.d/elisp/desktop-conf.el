e1198c65b1dc0d53c4da1b4d12ce91695e02bf2b
Raphaël Cauderlier <cauderlier@crans.org>
2014-06-18 15:51:50 +0200

    [Desktop] Save compile history

--- .emacs.d/elisp/desktop-conf.el
+++ .emacs.d/elisp/desktop-conf.el
@@ -3,5 +3,5 @@
 (desktop-save-mode 1)
 (add-to-list 'desktop-globals-to-save 'command-history)
 (add-to-list 'desktop-globals-to-save 'shell-command-history)
-
+(add-to-list 'desktop-globals-to-save 'compile-history)
 (provide 'desktop-conf)
