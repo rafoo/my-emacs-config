5a6967eb7926408dd9587e61de6ced1239a23b32
Raphaël Cauderlier <cauderlier@crans.org>
2014-06-18 16:41:26 +0200

    Keybindings to jump to source code

--- .emacs.d/elisp/editing-conf.el
+++ .emacs.d/elisp/editing-conf.el
@@ -21,4 +21,11 @@
 
 (require 'coq-conf)
 
+;; Bind keys to functions jumping to source code
+;; Idea from http://emacsredux.com/blog/2014/06/18/quickly-find-emacs-lisp-sources/
+(define-key 'help-command (kbd "C-l") 'find-library)
+(define-key 'help-command (kbd "C-f") 'find-function)
+(define-key 'help-command (kbd "C-k") 'find-function-on-key)
+(define-key 'help-command (kbd "C-v") 'find-variable)
+
 (provide 'editing-conf)
