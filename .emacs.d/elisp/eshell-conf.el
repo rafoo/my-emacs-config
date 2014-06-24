1b9e1bd83168aca1f338ab192beaee085de9f38c
Raphaël Cauderlier <cauderlier@crans.org>
2014-06-24 20:22:38 +0200

    [eshell-conf] Cool functions to quickly pop and kill eshell

--- .emacs.d/elisp/eshell-conf.el
+++ .emacs.d/elisp/eshell-conf.el
@@ -1,3 +1,10 @@
+;;; eshell-conf.el -- configuration for Eshell
+
+;;; Commentary:
+;; Not part of Emacs.
+
+;;; Code:
+
 (setq eshell-modules-list '(eshell-alias
                             eshell-basic
                             eshell-cmpl
@@ -16,4 +23,37 @@
 
 (global-set-key (kbd "C-c s") 'eshell)
 
+
+
+(defvar eshell-before-wconf nil
+  "Window configuration recorded by `eshell-in-other-window'
+and restored by `eshell-exit'.")
+
+(defun eshell-in-other-window ()
+  (interactive)
+  (setq eshell-before-wconf (current-window-configuration))
+  (with-current-buffer (pop-to-buffer nil)
+    (eshell)))
+
+(global-set-key (kbd "<s-return>") 'eshell-in-other-window)
+
+(defun eshell-exit ()
+  (interactive)
+  (kill-buffer (current-buffer))
+  (when eshell-before-wconf
+    (set-window-configuration eshell-before-wconf))
+  (setq eshell-before-wconf nil))
+
+;; C-d in eshell exit
+(add-hook 'eshell-mode-hook
+          (lambda ()
+            ;; I don't know how to do this whitout local-set-key
+            ;; because eshell-mode-map is buffer-local
+            ;; (and I don't know why).
+            (local-set-key
+              (kbd "C-d")
+              'eshell-exit)))
+
+
 (provide 'eshell-conf)
+;;; eshell-conf.el ends here
