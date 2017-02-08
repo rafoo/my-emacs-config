623ea3acc21f5c9ca603c45706c6e75751f0e525
Raphael Cauderlier <cauderlier@liafa.univ-paris-diderot.fr>
2017-02-08 10:47:04 +0100

    Remove some keybindings that I have not used for a long time

--- .emacs.d/elisp/persp-conf.el
+++ .emacs.d/elisp/persp-conf.el
@@ -19,8 +19,8 @@
   (kbd "C-c <menu>"))
 
 (define-persp-app "packages" (list-packages) (kbd "C-c p"))
-(define-persp-app "gnus" (gnus) (kbd "C-c n"))
-(define-persp-app "erc" () (kbd "C-c i") (erc))
+(define-persp-app "gnus" (gnus) (kbd "C-c n")) ;; News
+(define-persp-app "erc" () (kbd "C-c i") (erc)) ;; IRC
 (define-persp-app "agenda" (org-agenda) (kbd "C-c a"))
 (define-persp-app "org" (dired (concat my-home "/git/org")) (kbd "C-c o"))
 (define-persp-app "xkcd" (xkcd) (kbd "C-c x"))
@@ -32,14 +32,6 @@
 
 (define-persp-with-shell-process "wicd" "wicd-gtk" (kbd "C-c w"))
 (define-persp-with-shell-process "web" "tbb" (kbd "C-c b"))
-
-(define-persp-magit-app "these" (kbd "C-c t"))
-(define-persp-magit-app "dedukti" (kbd "C-c d"))
-(define-persp-magit-app "sigmaid" (kbd "C-c ç"))
-(define-persp-magit-app "expressing" (kbd "C-c e"))
-(define-persp-magit-app "focalize" (kbd "C-c f"))
-(define-persp-magit-app "holide" (kbd "C-c h"))
-(define-persp-magit-app "zenon" (kbd "C-c z"))
 
 (provide 'persp-conf)
 ;;; persp-conf.el ends here
