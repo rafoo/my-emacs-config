f66f12df0575485a97f52a5d9d95af4d845a1259
Raphael Cauderlier <cauderlier@liafa.univ-paris-diderot.fr>
2017-10-31 17:54:06 +0100

    Add a docstring

--- .emacs.d/elisp/offlineimap-conf.el
+++ .emacs.d/elisp/offlineimap-conf.el
@@ -2,6 +2,7 @@
 ;; Give password to OfflineImap
 ;; Source: http://www.emacswiki.org/emacs/OfflineIMAP#toc2
 (defun offlineimap-get-password (host port)
+  "Give password to OfflineImap"
   (message "OfflineImap Get Password")
   (let ((result (car (auth-source-search :host host :port port))))
     (funcall (plist-get result :secret))))
