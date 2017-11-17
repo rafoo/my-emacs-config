df3c6d581dd6a3ca1d5a5934354d8061a4989c40
Raphael Cauderlier <cauderlier@liafa.univ-paris-diderot.fr>
2017-11-17 16:19:56 +0100

    [header-line] Fix some linting warnings

--- .emacs.d/elisp/header-line-conf.el
+++ .emacs.d/elisp/header-line-conf.el
@@ -1,14 +1,23 @@
-;; Configuration of the header-line to display global informations such as time, battery, ...
+;;; header-line-conf --- Configuration of the header-line to display global informations such as time, battery, ...
 
-(setq global-header-string nil)
+;;; Commentary:
+;; The mode line is often not wide enough to display all the
+;; information we want.  The header line on the opposite is seldom
+;; used.  This package moves some information from the mode line to
+;; the header line.
+;;; Code:
+
+(defvar global-header-string nil
+  "Play the role of `global-mode-string' but for header line instead of mode line.")
+
 (defmacro var-subst (var1 var2 exp)
+  "Run EXP acting on variable VAR1 instead of VAR2."
   `(let ((,var1 ,var2))
         ,exp
         (setq ,var2 ,var1) ) )
 
 (defun apply-header (fun &rest args)
-  "Apply function FUN with arguments ARGS using `global-header-string'
-in place of `global-mode-string'."
+  "Apply function FUN with arguments ARGS using `global-header-string' in place of `global-mode-string'."
   (var-subst global-mode-string global-header-string (apply fun args)) )
 
 ;;; Global information
@@ -30,14 +39,15 @@ in place of `global-mode-string'."
 ;;; More header-line
 
 (defun my-header-line (format)
-  "Concatenation of the argument FORMAT seen as a mode-line-format with some fixed information"
+  "Concatenation of the argument FORMAT seen as a `mode-line-format' with some fixed information."
   (list "" format
         '(global-header-string (" " global-header-string)) ) )
 
 (defun append-header ()
-  (interactive)
+  "Add global information to the end of the header line."
   (setq header-line-format (my-header-line header-line-format)) )
 
 (add-hook 'after-change-major-mode-hook 'append-header)
 
 (provide 'header-line-conf)
+;;; header-line-conf.el ends here
