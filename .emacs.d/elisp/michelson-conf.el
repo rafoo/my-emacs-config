ee2fb9e0ea3f40748574c7a8cdd4d313a2e2e8a4
Raphaël Cauderlier <raphael.cauderlier@nomadic-labs.com>
2020-07-18 16:35:53 +0200

    Don't hardcode a tezos client path in michelson-config

--- .emacs.d/elisp/michelson-conf.el
+++ .emacs.d/elisp/michelson-conf.el
@@ -1,5 +1,4 @@
 (setq michelson-client-command "~/git/tezos/tezos-client --mode mockup --protocol ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK")
 (setq michelson-alphanet nil)
 
-(setq michelson-client-command "/tmp/tezos-tmp-client.tMzeKizY/bin/tezos-client")
 (provide 'michelson-conf)
